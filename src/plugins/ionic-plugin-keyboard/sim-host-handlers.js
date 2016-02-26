e31cf6350f33e604e2b3b2bb8833246098c18c3a
Tim Barham <tim.barham@microsoft.com>
2016-02-26 16:11:50 +1000

    Don't show "unhandled exec call" for Keyboard.init

---
+++ src/plugins/ionic-plugin-keyboard/sim-host-handlers.js
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+module.exports = {
+    'Keyboard': {
+        'init': function (successCallback) {
+            // Report to the app the keyboard is hidden
+            successCallback("H");
+        }
+    }
+};
