142351e94a6be84e04eda3726e01ecc246571dd2
Arzhan Kinzhalin <arzhan.i.kinzhalin@intel.com>
2016-06-09 11:11:25 -0700

    Add JSON linting.
    Also reorganize gulp tasks accordingly. JSON linting is included into the default
    gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
-var gulp = require('gulp'),
-    eslint = require('gulp-eslint');
+var gulp        = require('gulp'),
+    eslint      = require('gulp-eslint'),
+    jsonlint    = require('gulp-jsonlint');
 
-gulp.task('lint', function () {
+gulp.task('eslint', function () {
     return gulp.src('src/**/*.js')
         .pipe(eslint())
-        .pipe(eslint.format());
-        //.pipe(eslint.failOnError());
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
+
+gulp.task('jsonlint', function () {
+    return gulp.src([ 'src/**/*.json', 'package.json' ])
+        .pipe(jsonlint())
+        .pipe(jsonlint.reporter())
+        .pipe(jsonlint.failAfterError());
+});
+
+gulp.task('lint', [ 'eslint', 'jsonlint' ]);
 
 gulp.task('default', [ 'lint' ]);
