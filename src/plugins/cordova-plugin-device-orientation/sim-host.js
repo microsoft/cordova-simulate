217e803ebbceb19829bf67f2550dd6e6723f02c3
Albertina Durante <albertina.durante@intel.com>
2016-06-28 19:51:26 -0300

    Several compass simulation fixes (#100)
    Fixing displaying the heading value on initialize [#86]
    Validating compass maximum and minimum values. [#71]
    - Checking limits values according to the plugin spec
    - Updating input number step to 0.5 to be consistent with
    geolocation simulation control

--- src/plugins/cordova-plugin-device-orientation/sim-host.js
+++ src/plugins/cordova-plugin-device-orientation/sim-host.js
@@ -30,7 +30,16 @@ module.exports = function (messages) {
             },
             sendUITelemetry: sendUITelemetry
         });
+
+        var updateHeadingValue = function () {
+            var heading = compassWidget.heading();
+            compass.updateHeading(heading.value);
+            inputHeading.value = heading.value;
+            headingText.textContent = heading.direction;
+        };
+
         compassWidget.initialize(compass.heading);
+        updateHeadingValue();
 
         inputHeading.addEventListener('change', function () {
             compassWidget.updateHeading(this.value);
@@ -38,13 +47,15 @@ module.exports = function (messages) {
             sendUITelemetry('compass-heading-value');
         });
 
+        inputHeading.addEventListener('input', function () {
+            if (this.value < compass.Limits.MIN || this.value >= compass.Limits.MAX) {
+                this.value = compass.heading;
+            }
+        });
+
         messages.on('device-orientation-updated', function (event, value) {
             compassWidget.setHeading(value);
-
-            var heading = compassWidget.heading();
-            compass.updateHeading(heading.value);
-            inputHeading.value = heading.value;
-            headingText.textContent = heading.direction;
+            updateHeadingValue();
         }, true); // global event
     }
 
