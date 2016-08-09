147f4b4c804989d0e990332d16be83799552d2c5
Albertina Durante <albertina.durante@intel.com>
2016-08-09 13:53:54 -0300

    Updating sim hosts file injection while streaming app host.
    - Update regex to support matching head with content like data-*
    attributes
    - Using captures for inserting the new head: do not replace it
    with a new one, take the current head and add the script files

--- src/server/server.js
+++ src/server/server.js
@@ -223,7 +223,7 @@ SimulationServer.prototype._streamAppHostHtml = function (request, response) {
             send(request, filePath, {
                 transform: function (stream) {
                     return stream
-                        .pipe(replaceStream(/<\s*head\s*>/, '<head>' + scriptTags))
+                        .pipe(replaceStream(/(<\s*head.*>)/, '$1' + scriptTags))
                         .pipe(replaceStream('default-src \'self\'', 'default-src \'self\' ws: blob:'));
                 }
             }).pipe(response);
