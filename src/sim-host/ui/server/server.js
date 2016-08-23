c019b9a10611070de0efa0314ac8e3cff73a3eb8
TimBarham <tim.barham@microsoft.com>
2016-08-23 13:56:08 -0700

    Rendering improvements in non-Chrome browsers.

--- src/sim-host/ui/server/server.js
+++ src/sim-host/ui/server/server.js
@@ -4,22 +4,35 @@ var path = require('path'),
     replaceStream = require('replacestream'),
     send = require('send-transform');
 
+var BROWSER_REPLACE_MAX_MATCH_LENGTH = 1024;
+var BROWSER_REPLACE_OPTIONS = {maxMatchLen: BROWSER_REPLACE_MAX_MATCH_LENGTH};
+
 module.exports.attach = function (app, dirs, hostRoot) {
-    app.get('/simulator/sim-host.css', function (request, response, next) {
+    app.get('/simulator/sim-host.css', function (request, response) {
         var userAgent = request.headers['user-agent'];
-        if (userAgent.indexOf('Chrome') > -1 && userAgent.indexOf('Edge/') === -1) {
-            next();
-        } else {
-            // If target browser isn't Chrome (user agent contains 'Chrome', but isn't 'Edge'), remove shadow dom stuff from
-            // the CSS file. Also remove any sections marked as Chrome specific.
-            send(request, path.resolve(hostRoot['sim-host'], 'sim-host.css'), {
-                transform: function (stream) {
-                    return stream
-                        .pipe(replaceStream('> ::content >', '>'))
-                        .pipe(replaceStream(/\^|\/shadow\/|\/shadow-deep\/|::shadow|\/deep\/|::content|>>>/g, ' '))
-                        .pipe(replaceStream(/\/\* BEGIN CHROME ONLY \*\/[\s\S]*\/\* END CHROME ONLY \*\//gm, ''));
-                }
-            }).pipe(response);
-        }
+        send(request, path.resolve(hostRoot['sim-host'], 'sim-host.css'), {
+            transform: getTransform(userAgent)
+        }).pipe(response);
     });
 };
+
+function getTransform(userAgent) {
+    if (isChrome(userAgent)) {
+        // If target browser is Chrome, remove any sections marked as not for Chrome.
+        return function (stream) {
+            return stream.pipe(replaceStream(/\/\* BEGIN !CHROME \*\/[\s\S]*\/\* END !CHROME \*\//gm, '', BROWSER_REPLACE_OPTIONS));
+        };
+    }
+
+    // If target browser is not Chrome, remove shadow dom stuff and any sections marked as for Chrome.
+    return function (stream) {
+        return stream
+            .pipe(replaceStream('> ::content >', '>'))
+            .pipe(replaceStream(/\^|\/shadow\/|\/shadow-deep\/|::shadow|\/deep\/|::content|>>>/g, ' '))
+            .pipe(replaceStream(/\/\* BEGIN CHROME \*\/[\s\S]*\/\* END CHROME \*\//gm, '', BROWSER_REPLACE_OPTIONS));
+    };
+}
+
+function isChrome(userAgent) {
+    return userAgent.indexOf('Chrome') > -1 && userAgent.indexOf('Edge/') === -1;
+}
