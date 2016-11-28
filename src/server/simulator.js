71308fb2a8b7bc58e0a6cc56e789607214e0ae0c
Jimmy Thomson <jithomso@microsoft.com>
2016-11-28 10:51:08 -0800

    Automatically trigger a retheme
    When updating the theme on the Simulator object, automatically propagate the change to the sim host

--- src/server/simulator.js
+++ src/server/simulator.js
@@ -204,6 +204,9 @@ Simulator.prototype.updateDevice = function (newDevice) {
 
 Simulator.prototype.updateTheme = function (themeData) {
     this._config.theme = theme.createTheme(this.hostRoot['sim-host'], themeData);
+    if (this._server.simSocket) {
+        this._server.simSocket.rethemeSimHost();
+    }
 }
 
 /**
