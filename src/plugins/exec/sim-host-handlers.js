d9567284ce770c238bee099a76e29d244c6d3afb
Dmitrii Snytkin <v-dmsnyt@microsoft.com>
2017-04-28 12:48:57 +0300

    Fix lint problem

--- src/plugins/exec/sim-host-handlers.js
+++ src/plugins/exec/sim-host-handlers.js
@@ -35,7 +35,7 @@ module.exports = {
                     errorDisplay.style.display = 'none';
                     document.getElementById('exec-service').textContent = service;
                     document.getElementById('exec-action').textContent = action;
-                    document.getElementById('exec-args').textContent = (args || []).map(JSON.stringify).join(", ");
+                    document.getElementById('exec-args').textContent = (args || []).map(JSON.stringify).join(', ');
 
                     /*eslint-disable no-inner-declarations */
                     function handleSuccess() {
