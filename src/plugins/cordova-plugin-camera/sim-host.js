19b7db7c0b2d567c6406fd7e0766bf03c19a0d43
Pavel Yakovlev (Akvelon) <v-payako@microsoft.com>
2017-05-02 17:38:53 +0300

    - added support host system camera and default camera image

--- src/plugins/cordova-plugin-camera/sim-host.js
+++ src/plugins/cordova-plugin-camera/sim-host.js
@@ -16,7 +16,30 @@ module.exports = function (messages) {
         preSelectedFile;
 
     messages.register('takePicture', function (args, callback) {
-        if (document.getElementById('camera-prompt').checked) {
+        if (document.getElementById('camera-host').checked) {
+            navigator.mediaDevices.getUserMedia({ video: true}).then(function(stream) {
+                var video = document.getElementById('camera-stream');
+                video.src = window.URL.createObjectURL(stream);
+                video.play();
+                video.onplaying = function() {
+                    var canvas = document.getElementById('camera-host-canvas');
+                    var width = video.videoWidth;
+                    var height = video.videoHeight;
+
+                    if (width && height) {
+                        canvas.width = width;
+                        canvas.height = height;
+                    }
+
+                    canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+                    video.pause();
+
+                    getPictureFromCanvas(canvas, callback, args);
+                };
+            }).catch(function(err){
+                window.alert('There was an error with accessing the camera stream: ' + err.name, err);
+            });
+        } else if (document.getElementById('camera-prompt').checked) {
             dialog.showDialog('camera-choose-image', function (msg) {
                 if (msg === 'showing') {
                     // Not we use .onclick etc here rather than addEventListener() to ensure we replace any existing
@@ -35,10 +58,27 @@ module.exports = function (messages) {
                     };
                 }
             });
-        } else if (document.getElementById('camera-file').checked) {
+        } else if (document.getElementById('camera-sample').checked) {
+            var canvas = document.getElementById('camera-sample-canvas');
+            if (canvas.getAttribute('done')) {
+                getPictureFromCanvas(canvas, callback, args);
+            }
+        }
+        else if (document.getElementById('camera-file').checked) {
             getPicture(preSelectedFile, callback, args);
         }
     });
+
+    function getPictureFromCanvas(canvas, callback, args) {
+        if (args && args[1] === 0) {
+            /* Destination type is DATA_URL */
+            callback(null, canvas.toDataURL('image/png'));
+        } else {
+            canvas.toBlob(function(blob) { 
+                createArrayBuffer(blob, callback);
+            }, 'image/png');
+        }
+    }
 
     function getPicture(file, callback, args) {
         if (args && args[1] === 0) {
@@ -82,6 +122,36 @@ module.exports = function (messages) {
                 telemetry.sendUITelemetry(Object.assign({}, baseProps, { control: 'camera-choose-filename' }));
                 filenameInput.input.click();
             });
+            
+            document.getElementById('camera-make-sample').addEventListener('click', function () {
+                navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+                    var video = document.getElementById('camera-stream');
+                    video.src = window.URL.createObjectURL(stream);
+                    video.play();
+                    video.onplaying = function() {
+                        var canvas = document.getElementById('camera-sample-canvas');
+                        var width = video.videoWidth;
+                        var height = video.videoHeight;
+
+                        if (width && height) {
+                            canvas.width = width;
+                            canvas.height = height;
+                        }
+
+                        canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+                        canvas.setAttribute('done', true);
+                        video.pause();
+                        
+                        // preview
+                        var data = canvas.toDataURL('image/png');
+                        var panelImg = document.getElementById('camera-img');
+                        panelImg.setAttribute('src', data);
+                        panelImg.style.display = 'block';
+                    };
+                }).catch(function(err){
+                    window.alert('There was an error with accessing the camera stream: ' + err.name, err);
+                });
+            });
 
             filenameInput.addEventListener('change', function () {
                 if (filenameInput.files[0]) {
@@ -117,6 +187,14 @@ module.exports = function (messages) {
 
             document.getElementById('camera-prompt').onclick = handleRadioClick.bind(this, 'camera-prompt');
             document.getElementById('camera-file').onclick = handleRadioClick.bind(this, 'camera-file');
+            document.getElementById('camera-host').onclick = handleRadioClick.bind(this, 'camera-host');
+            document.getElementById('camera-sample').onclick = handleRadioClick.bind(this, 'camera-sample');
+
+            // hide actions if they are not supported by the browser (IE 11, Safari, Opera mini)
+            if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+                document.getElementById('camera-host').style.display = 'none';
+                document.getElementById('camera-sample-row').style.display = 'none';
+            }
         }
     };
 };
