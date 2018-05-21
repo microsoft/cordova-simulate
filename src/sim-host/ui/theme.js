799518d5d1bf9f6debb2864160ca0b42985c4366
Artem Egorov <toysruss89@gmail.com>
2018-05-21 15:03:45 +0300

    Fixed renderind in Chrome

--- src/sim-host/ui/theme.js
+++ src/sim-host/ui/theme.js
@@ -3,17 +3,17 @@
 module.exports = {
     elementSelectors: {
         'body': 'body',
-        'input': ['cordova-combo /deep/ select, body /deep/ textarea, body /deep/ input[type^=text], body /deep/ input[type^=number], body /deep/ input[type^=checkbox]',
+        'input': ['textarea, input[type^=text], input[type^=number], input[type^=checkbox]',
             'input[type=range]::-ms-fill-lower, input[type=range]::-ms-fill-upper',
             'input[type=range]::-webkit-slider-runnable-track',
             'input[type=range]::-moz-range-track'],
         'thumb': ['input[type=range]::-ms-thumb', 'input[type=range]::-webkit-slider-thumb','input[type=range]::-moz-range-thumb'],
         'check': '::-ms-check',
-        'button': 'body /deep/ button, body /deep/ .cordova-item-wrapper',
-        'label': 'body /deep/ label',
-        'value': 'body /deep/ .cordova-value',
-        'panel': 'body /deep/ .cordova-panel-inner',
-        'panel-caption': 'body /deep/ .cordova-panel-inner%state% .cordova-header'
+        'button': 'button, .cordova-item-wrapper',
+        'label': 'label',
+        'value': '.cordova-value',
+        'panel': '.cordova-panel-inner',
+        'panel-caption': '.cordova-panel-inner%state% .cordova-header'
     },
 
     defaultTheme: {
