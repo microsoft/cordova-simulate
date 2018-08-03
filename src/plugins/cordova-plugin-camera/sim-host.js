83add8dbe0f2d27ba2b884bac28bc83d127739ce
Pavel Yakovlev <v-payako@microsoft.com>
2018-08-03 13:28:02 +0300

    fixes #47 - Added support host system camera and default camera image (#234)
    * - added support host system camera and default camera image
    
    * Improve implementation
    
    * Fix styles

--- src/plugins/cordova-plugin-camera/sim-host.js
+++ src/plugins/cordova-plugin-camera/sim-host.js
@@ -16,10 +16,16 @@ module.exports = function (messages) {
         preSelectedFile;
 
     messages.register('takePicture', function (args, callback) {
-        if (document.getElementById('camera-prompt').checked) {
+        var canvas;
+        if (document.getElementById('camera-host').checked) {
+            canvas = document.getElementById('camera-host-canvas');
+            takeSampleFromCamera(canvas, function (canvas) {
+                getPictureFromCanvas(canvas, callback, args);
+            });
+        } else if (document.getElementById('camera-prompt').checked) {
             dialog.showDialog('camera-choose-image', function (msg) {
                 if (msg === 'showing') {
-                    // Not we use .onclick etc here rather than addEventListener() to ensure we replace any existing
+                    // Now we use .onclick etc here rather than addEventListener() to ensure we replace any existing
                     // handler with one that uses the appropriate value of 'callback' from the current closure.
                     document.getElementById('camera-dialog-use-image').onclick = function () {
                         dialog.hideDialog('camera-choose-image');
@@ -35,10 +41,62 @@ module.exports = function (messages) {
                     };
                 }
             });
-        } else if (document.getElementById('camera-file').checked) {
+        } else if (document.getElementById('camera-sample').checked) {
+            var cameraSampleImg = document.getElementById('camera-sample-img');
+            if (cameraSampleImg.getAttribute('src')) {
+                canvas = document.getElementById('camera-sample-canvas');
+                getPictureFromCanvas(canvas, callback, args);
+            }
+        }
+        else if (document.getElementById('camera-file').checked) {
             getPicture(preSelectedFile, callback, args);
         }
     });
+
+    function takeSampleFromCamera(canvas, callback) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+            var video = document.getElementById('camera-stream');
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+            video.onplaying = function () {
+                var width = video.videoWidth;
+                var height = video.videoHeight;
+
+                if (width && height) {
+                    canvas.width = width;
+                    canvas.height = height;
+                }
+
+                canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+                video.pause();
+
+                callback(canvas);
+            };
+        }).catch(function (error) {
+            var errorMessage = 'There was an error with accessing the camera stream: ' + error.name;
+            if (error.message) {
+                errorMessage += ', ' + error.message;
+            }
+            console.error(error);
+            window.alert(errorMessage);
+        });
+    }
+
+    function getPictureFromCanvas(canvas, callback, args) {
+        if (args && args[1] === 0) {
+            /* Destination type is DATA_URL */
+            // TODO: Add support for Camera.EncodingType instead of hardcoded image/jpeg
+            var imageData = canvas.toDataURL('image/jpeg');
+            if (imageData) {
+                imageData = imageData.substr(imageData.indexOf(',') + 1);
+            }
+            callback(null, imageData);
+        } else {
+            canvas.toBlob(function (blob) {
+                createArrayBuffer(blob, callback);
+            }, 'image/jpeg');
+        }
+    }
 
     function getPicture(file, callback, args) {
         if (args && args[1] === 0) {
@@ -60,6 +118,7 @@ module.exports = function (messages) {
     function createDataUrl(blob, callback) {
         var reader = new FileReader();
         reader.onloadend = function () {
+            // TODO: Add support for Camera.EncodingType instead of hardcoded image/jpeg
             var imageData = reader.result;
             if (imageData) {
                 imageData = imageData.substr(imageData.indexOf(',') + 1);
@@ -81,6 +140,17 @@ module.exports = function (messages) {
             document.getElementById('camera-choose-filename').addEventListener('click', function () {
                 telemetry.sendUITelemetry(Object.assign({}, baseProps, { control: 'camera-choose-filename' }));
                 filenameInput.input.click();
+            });
+
+            document.getElementById('camera-make-sample').addEventListener('click', function () {
+                var canvas = document.getElementById('camera-sample-canvas');
+                takeSampleFromCamera(canvas, function (canvas) {
+                    // preview
+                    var data = canvas.toDataURL('image/jpeg');
+                    var panelImg = document.getElementById('camera-sample-img');
+                    panelImg.setAttribute('src', data);
+                    panelImg.style.display = 'block';
+                });
             });
 
             filenameInput.addEventListener('change', function () {
@@ -121,6 +191,14 @@ module.exports = function (messages) {
 
             document.getElementById('camera-prompt').onclick = handleRadioClick.bind(this, 'camera-prompt');
             document.getElementById('camera-file').onclick = handleRadioClick.bind(this, 'camera-file');
+            document.getElementById('camera-host').onclick = handleRadioClick.bind(this, 'camera-host');
+            document.getElementById('camera-sample').onclick = handleRadioClick.bind(this, 'camera-sample');
+
+            // hide actions if they are not supported by the browser (IE 11, Safari, Opera mini)
+            if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+                document.getElementById('camera-host').style.display = 'none';
+                document.getElementById('camera-sample-options').style.display = 'none';
+            }
         }
     };
 };
