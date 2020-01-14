e20c5036f2111cca553cbf8e120b6c2f2bc837a7
Max Fisher <maxmfishernj@gmail.com>
2020-01-14 15:17:34 -0800

    fixed missing return from filter function and inproved redablity

--- src/devices/make-uuids.js
+++ src/devices/make-uuids.js
@@ -1,16 +1,15 @@
 var fs = require('fs');
 var uuidv4 = require('uuid/v4');
-var path = require('path')
-var deviceList = require(path.resolve('src/devices/devices.json'));
 
-var UUIDBlacklist = ['android']
+var deviceList = require('./devices.json');
+var UUIDPlatformBlacklist = ['android']
 
-Object.keys(deviceList).filter(function(platform) {!UUIDBlacklist.includes(platform)} ).map(function(platform) {
+Object.keys(deviceList).filter(function(platform) {
+    return !UUIDPlatformBlacklist.includes(platform);
+}).map(function(platform) {
     deviceList[platform] = deviceList[platform].map(function(device) {
-        device.uuid = uuidv4();
-        return device;
+        return Object.assign(device,{uuid:uuidv4()});
     });
 });
 
-
-fs.writeFileSync(path.resolve('src/devices/devices.json'),JSON.stringify(deviceList))
+fs.writeFileSync('./src/devices/devices.json',JSON.stringify(deviceList,null,4))
