2b7d6717e9218cd5b30116084267a5305519d58f
Max Fisher <maxmfishernj@gmail.com>
2020-01-16 14:29:57 -0800

    Requested changes

--- src/devices/make-uuids.js
+++ src/devices/make-uuids.js
@@ -1,16 +1,17 @@
 var fs = require('fs');
+var path = require('path');
 var uuidv4 = require('uuid/v4');
 
 var deviceList = require('./devices.json');
-var UUIDPlatformBlacklist = ['android']
+var UUIDPlatformBlacklist = ['android'];
 
 Object.keys(deviceList).filter(function(platform) {
     return !UUIDPlatformBlacklist.includes(platform);
 }).map(function(platform) {
     deviceList[platform] = deviceList[platform].map(function(device) {
-        return Object.assign(device,{uuid:uuidv4()});
+        return Object.assign(device, {uuid:uuidv4()});
     });
 });
 
-// eslint-disable-next-line no-undef
-fs.writeFileSync(__dirname+'/devices.json',JSON.stringify(deviceList,null,2))
+/*global __dirname */
+fs.writeFileSync(path.join(__dirname, '/devices.json'), JSON.stringify(deviceList, null, 2));
