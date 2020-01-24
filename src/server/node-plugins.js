b0ead36cfd184425206eeda4685246625270856a
Max Fisher <maxmfishernj@gmail.com>
2020-01-23 22:16:31 -0800

    var -> let

--- src/server/node-plugins.js
+++ src/server/node-plugins.js
@@ -1,17 +1,17 @@
 /* eslint-env es6 */
-var fs = require('fs');
-var path = require('path');
+let fs = require('fs');
+let path = require('path');
 
 // iife to allow plugin gathering when module is required
 // Intentional use of synchronous fs calls to stop plugins from being exported until all plugins are checked
-var plugins = {};
+let plugins = {};
 (function () {
-    var cordovaPlugins = fs.readdirSync(path.join(process.cwd(), 'plugins'));
-    for(var cordovaPlugin of cordovaPlugins) {
+    let cordovaPlugins = fs.readdirSync(path.join(process.cwd(), 'plugins'));
+    for(let cordovaPlugin of cordovaPlugins) {
         try {
-            var temp = require(path.join(process.cwd(), 'plugins', cordovaPlugin, 'src/simulation/simulate_gap.js'));
-            var services = Object.keys(temp);
-            for(var service of services) {
+            let temp = require(path.join(process.cwd(), 'plugins', cordovaPlugin, 'src/simulation/simulate_gap.js'));
+            let services = Object.keys(temp);
+            for(let service of services) {
                 plugins[service] = temp[service];
             }
         // eslint-disable-next-line no-empty
