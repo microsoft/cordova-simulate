1d3a0a7051644bec332196e2b45fd5cc64debcb8
benjaminbi <bi.crystal@gmail.com>
2023-01-17 16:01:34 +0800

    Create browser helpers for simulate browser supporting Fixes #388

--- src/helpers/browser.js
+++ src/helpers/browser.js
@@ -1,22 +1,3 @@
-/**
- Licensed to the Apache Software Foundation (ASF) under one
- or more contributor license agreements.  See the NOTICE file
- distributed with this work for additional information
- regarding copyright ownership.  The ASF licenses this file
- to you under the Apache License, Version 2.0 (the
- "License"); you may not use this file except in compliance
- with the License.  You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on an
- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- KIND, either express or implied.  See the License for the
- specific language governing permissions and limitations
- under the License.
- */
-
 /* globals Promise: true */
 
 const child_process = require('child_process');
@@ -32,7 +13,7 @@ const NOT_SUPPORTED = 'The browser target is not supported: %target%';
  * Launches the specified browser with the given URL.
  * Based on https://github.com/domenic/opener
  * @param {{target: ?string, url: ?string, dataDir: ?string}} opts - parameters:
- *   target - the target browser - ie, chrome, safari, opera, firefox or chromium
+ *   target - the target browser - chrome, safari, firefox or chromium
  *   url - the url to open in the browser
  *   dataDir - a data dir to provide to Chrome (can be used to force it to open in a new window)
  * @return {Promise} Promise to launch the specified browser
@@ -102,24 +83,19 @@ function getBrowser (target, dataDir) {
     const chromeArgs = ` --user-data-dir=/tmp/${dataDir}`;
     const browsers = {
         win32: {
-            ie: 'iexplore',
             chrome: `chrome --user-data-dir=%TEMP%\\${dataDir}`,
-            safari: 'safari',
-            opera: 'opera',
             firefox: 'firefox',
             edge: 'microsoft-edge'
         },
         darwin: {
             chrome: `"Google Chrome" --args${chromeArgs}`,
             safari: 'safari',
-            firefox: 'firefox',
-            opera: 'opera'
+            firefox: 'firefox'
         },
         linux: {
             chrome: `google-chrome${chromeArgs}`,
             chromium: `chromium-browser${chromeArgs}`,
-            firefox: 'firefox',
-            opera: 'opera'
+            firefox: 'firefox'
         }
     };
 
