561ce150b88a22c31e940dedc223a75220201f61
Ezio Li <v-yukl@microsoft.com>
2023-02-03 13:23:42 +0800

    Update

--- test/server.test.js
+++ test/server.test.js
@@ -1,21 +1,17 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
-const assert = require("assert");
+const assert = require('assert');
+const path = require('path');
+var Simulator = require('../src/server/simulator');
 
-suite("simulateServer", function () {
-    test("Simulator server can be started successfully.", (done) => {
-        var Simulator = require('../src/server/simulator');
+suite('simulatorProcess', function () {
+    test('After simulator init, simulator.state should be IDLE', (done) => {
         const options = require('./resources/options.json');
+        options.dir = path.join(__dirname, './resources/testSampleProject');
         var simulator = new Simulator(options);
-        assert.equal(simulator._state, Simulator.State.IDLE, "New created simulator should in IDLE state.");
-
-        var start = simulator.startSimulation();
-        assert.equal(simulator._state, Simulator.State.STARTING, "Simulate server");
-
-
-        simulator._state = Simulator.State.RUNNING;
-        simulator.stopSimulation();
+        assert.equal(simulator._state, Simulator.State.IDLE);
         done();
     });
 });
+
