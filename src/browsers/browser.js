e563b2c9580633fd70e7b4e5680544520ff417ae
EzioLi <v-yukl@microsoft.com>
2023-11-09 10:21:21 +0800

    Handle showbrowser argument for cordova-tools

--- src/browsers/browser.js
+++ src/browsers/browser.js
@@ -18,6 +18,9 @@ function launchBrowser(opts) {
     const url = opts.url || '';
     let showBrowser = opts.showBrowser;
 
+    // Handle showbrowser argument sent from cordova-tools and other unknown scenarios
+    // Only showbrowser = false will return method
+    showBrowser = showBrowser != false ? true : false;
     if (!showBrowser) {
         return;
     }
