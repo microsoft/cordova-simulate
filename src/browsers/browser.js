e84a1e3476ff654fc626430bea290e6d8ef40bf6
lexie011 <155628155+lexie011@users.noreply.github.com>
2024-02-02 17:10:44 +0800

    Verify device and os version (#463)
    * [Unit Test] Verify device and os version
    
    * update chromium
    
    ---------
    
    Co-authored-by: Ezio Li <v-yukl@microsoft.com>

--- src/browsers/browser.js
+++ src/browsers/browser.js
@@ -79,7 +79,7 @@ function getBrowser(target, dataDir, url) {
         },
         darwin: {
             chrome: `"Google Chrome" --args${chromeArgs}`,
-            chromium: `chromium${chromeArgs}`,
+            chromium: 'chromium',
             safari: 'safari',
             firefox: 'firefox',
             opera: 'opera',
@@ -87,7 +87,7 @@ function getBrowser(target, dataDir, url) {
         },
         linux: {
             chrome: `google-chrome${chromeArgs}`,
-            chromium: `chromium-browser${chromeArgs}`,
+            chromium: 'chromium-browser',
             firefox: 'firefox',
             opera: 'opera',
             edge: `microsoft-edge ${url} --args${chromeArgs}`
